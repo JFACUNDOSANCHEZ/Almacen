b89e3e14eba4c6e618759fb1532b78a5610c229b
Facu Sanchez <facusanchz96@gmail.com>
2024-03-17 18:09:15 -0300

    tienda

--- src/componentes/CarruselMenu.jsx
+++ src/componentes/CarruselMenu.jsx
@@ -6,13 +6,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const CarouselMenu = () => {
-    
+
     const settings = {
         rtl: true,
         dots: false,
         infinite: true,
         speed: 1000, // Ajusta la velocidad a un valor más bajo
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000, // Aumenta el intervalo entre los cambios
@@ -20,21 +20,36 @@ const CarouselMenu = () => {
     };
 
 
+    const shadowColors = ['shadow-md shadow-green-200', 'shadow-md shadow-blue-200', 'shadow-md shadow-red-200', 'shadow-md shadow-yellow-200', 'shadow-md shadow-indigo-200'];
+
+    // Función para obtener un color de sombra aleatorio de la matriz
+    const getRandomShadowColor = () => {
+        return shadowColors[Math.floor(Math.random() * shadowColors.length)];
+    };
+
     return (
-        <div className=" font-bold ">
-
-
-            <Slider {...settings} >
-                <div> <p className=' bg-slate-100 border gap-5 m-5 text-center rounded-full text-gray-700  shadow-md shadow-green-200'> Lacteos</p>     </div>
-                <div> <p className=' bg-slate-100 border gap-5 m-5 text-center rounded-full text-gray-700  shadow-md shadow-green-200'> Gaseosa</p>     </div>
-                <div> <p className=' bg-slate-100 border gap-5 m-5 text-center rounded-full text-gray-700  shadow-md shadow-green-200'> Cigarrillos</p>     </div>
-                <div> <p className=' bg-slate-100 border gap-5 m-5 text-center rounded-full text-gray-700  shadow-md shadow-green-200'> Limpieza</p>     </div>
-                <div> <p className=' bg-slate-100 border gap-5 m-5 text-center rounded-full text-gray-700  shadow-md shadow-green-200'> Vinos</p>     </div>
-                <div> <p className=' bg-slate-100 border gap-5 m-5 text-center rounded-full text-gray-700  shadow-md shadow-green-200'> Fiambre</p>     </div>
-
+        <div className="font-bold">
+            <Slider {...settings}>
+                <div>
+                    <p className={`bg-slate-100 border gap-5 m-2 text-center rounded-full text-gray-700 p-1 ${getRandomShadowColor()}`}>Lacteos</p>
+                </div>
+                <div>
+                    <p className={`bg-slate-100 border gap-5 m-2 text-center rounded-full text-gray-700 p-1 ${getRandomShadowColor()}`}>Gaseosa</p>
+                </div>
+                <div>
+                    <p className={`bg-slate-100 border gap-5 m-2 text-center rounded-full text-gray-700 p-1 ${getRandomShadowColor()}`}>Cigarrillos</p>
+                </div>
+                <div>
+                    <p className={`bg-slate-100 border gap-5 m-2 text-center rounded-full text-gray-700 p-1 ${getRandomShadowColor()}`}>Limpieza</p>
+                </div>
+                <div>
+                    <p className={`bg-slate-100 border gap-5 m-2 text-center rounded-full text-gray-700 p-1 ${getRandomShadowColor()}`}>Vinos</p>
+                </div>
+                <div>
+                    <p className={`bg-slate-100 border gap-5 m-2 text-center rounded-full text-gray-700 p-1 ${getRandomShadowColor()}`}>Fiambre</p>
+                </div>
             </Slider>
         </div>
     );
 };
-
 export default CarouselMenu;
