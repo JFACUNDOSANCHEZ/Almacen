c6257701fde624bd9964a36b955c9220ac24ee05
Facu Sanchez <facusanchz96@gmail.com>
2024-03-17 21:11:52 -0300

    carusel

--- src/data.js
+++ src/data.js
@@ -2,15 +2,44 @@ const productos = [
   {
     id: 2,
     title: 'Papas',
-    categoria: '2',
+    categoria: '4',
     descripcion: 'Descripción del producto 2',
     precio: 19.99,
     medicion: 'c/u',
     promo: false,
     img:'https://www.almacenfamily.com/productos/7790310982426-59-5eff584e13526.jpg'
+  }, {
+    id: 2434,
+    title: 'Dr. Lemon',
+    categoria: '1',
+    descripcion: 'Descripción del producto 2',
+    precio: 19.99,
+    medicion: 'c/u',
+    promo: false,
+    img:'https://www.espaciovino.com.ar/media/default/0001/64/thumb_63837_default_big.jpeg'
   },
   {
-    id: 3,
+    id: 22434,
+    title: 'Queso rallado',
+    categoria: '2',
+    descripcion: 'Descripción del producto 2',
+    precio: 19.99,
+    medicion: 'c/u',
+    promo: false,
+    img:'https://arcordiezb2c.vteximg.com.br/arquivos/ids/164079/Queso-Rallado-La-Seren-sima-70-Gr-1-6867.jpg?v=637589358136530000'
+  },
+  {
+    id: 234,
+    title: 'Andes',
+    categoria: '1',
+    descripcion: 'Descripción del producto 2',
+    precio: 19.99,
+    medicion: 'c/u',
+    promo: false,
+    img:'https://gobar.vtexassets.com/arquivos/ids/157473/Andes-Origen-IPA.jpg?v=637218864324070000'
+  },
+  {
+    id: 3234,
     title: 'Gaseosa',
     categoria: '1',
     descripcion: 'Descripción del producto 3',
@@ -21,7 +50,7 @@ const productos = [
     {
       id: 1,
       title: 'Mayonesa',
-      categoria: '2',
+      categoria: '4',
       descripcion: 'Descripción del producto 1',
       precio: 10.99,
       medicion: 'c/u',
@@ -47,7 +76,7 @@ const productos = [
     }, {
       id: 65,
       title: 'Cebolla',
-      categoria: '2',
+      categoria: '4',
       descripcion: 'Descripción del producto 5',
       precio: 15.50,
       medicion: 'kg',
@@ -69,14 +98,24 @@ const productos = [
       medicion: 'c/u',
       img:'https://www.almacendelsol.com.ar/wp-content/uploads/2021/09/77976994.jpg'
     },
+  
     {
       id: 4336,
-      title: 'Salame',
-      categoria: '1',
+      title: 'Leche',
+      categoria: '2',
       descripcion: 'Descripción del producto 5',
       precio: 15.50,
       medicion: '100g',
-      img:'https://dcdn.mitiendanube.com/stores/001/147/470/products/salame-milan1-2d3402671455da5f9b16038207250983-640-0.jpg'
+      img:'https://modomarketar.vteximg.com.br/arquivos/ids/165628/Leche-Tregar-Entera-Caja-X-1-Lt-1-8231.jpg?v=638021793902930000'
+    },
+    {
+      id: 4336,
+      title: 'Leche',
+      categoria: '2',
+      descripcion: 'Descripción del producto 5',
+      precio: 15.50,
+      medicion: '100g',
+      img:'https://cdn11.bigcommerce.com/s-3stx4pub31/images/stencil/1280x1280/products/9059/25388/la-serenisima-dulce-de-leche-clasico-1-k__69767.1691505971.jpg?c=2'
     },
     // Agrega más productos según sea necesario
   ];
