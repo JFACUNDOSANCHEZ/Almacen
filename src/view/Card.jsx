787ddda4d910c268968dd63b70c00105089686a8
Facu Sanchez <facusanchz96@gmail.com>
2024-03-17 22:18:48 -0300

    tienda

--- src/view/Card.jsx
+++ src/view/Card.jsx
@@ -3,21 +3,21 @@ import React from 'react';
 const Card = ({ producto, addToCart }) => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center">
-    <div className="flex flex-col shadow-md md:flex-row w-64">
-      <div className="flex-1 p-4 rounded-lg h-full">
-        <div className="flex justify-end items-center">
-        <button onClick={() => addToCart(producto)} className="bg-black text-white text-lg px-3 py-1 rounded-full hover:bg-green-600">
-          +
-        </button>
-      </div>
-      <div className="flex-none flex justify-center items-center mx-auto h-25 mt-4 md:mt-0 md:w-24">
-          <img src={producto.img} alt="" className="w-32 h-32  md:w-24 md:h-24 object-contain" /> {/* Establece un tamaño fijo para la imagen */}
+      <div className="flex flex-col shadow-md md:flex-row w-48">
+        <div className="flex-1 p-2 rounded-lg h-full">
+          <div className="flex justify-end items-center">
+            <button onClick={() => addToCart(producto)} className="bg-black text-white text-lg px-3 py-1 rounded-full hover:bg-green-600">
+              +
+            </button>
+          </div>
+          <div className="flex-none flex justify-center items-center mx-auto h-20 mt-2 md:mt-0 md:w-16">
+            <img src={producto.img} alt="" className="w-24 h-24 md:w-16 md:h-16 object-contain" /> {/* Establece un tamaño fijo para la imagen */}
+          </div>
+          <h3 className="h-6 overflow-hidden">{producto.title}</h3> {/* Establece una altura fija para el título */}
+          <h3 className="text-sm font-semibold text-green-700 mb-2">$ {producto.precio} <span className='text-black text-xs font-light'>{producto.medicion}</span></h3>
         </div>
-        <h3 className="h-8 overflow-hidden">{producto.title}</h3> {/* Establece una altura fija para el título */}
-        <h3 className="text-lg font-semibold text-green-700 mb-3">$ {producto.precio} <span className='text-black text-1xl font-light'>{producto.medicion} </span></h3>
       </div>
     </div>
-  </div>
   );
 };
 
