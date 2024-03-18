fb3b98bbc6deb99931afbe078728b965b98750d0
Facu Sanchez <facusanchz96@gmail.com>
2024-03-17 22:14:52 -0300

    tienda

--- src/componentes/Carrusel.jsx
+++ src/componentes/Carrusel.jsx
@@ -16,18 +16,18 @@ const Carousel = () => {
     autoplaySpeed: 4000, // Intervalo de cambio automático en milisegundos (por ejemplo, 3 segundos)
   };
   return (
-    <div className="h-64 mb-8 md:h-[40%]">
+    <div className="h-44 mb-8 md:h-[40%]">
 
       <Slider {...settings}>
    
         <div>
-          <img src="https://i.ytimg.com/vi/N1NQuyJnWIk/sddefault.jpg" alt="Imagen 2" className='w-full h-64 mb-8 md:h-84 object-cover' />
+          <img src="https://i.ytimg.com/vi/N1NQuyJnWIk/sddefault.jpg" alt="Imagen 2" className='w-full h-44 mb-8 md:h-84 object-cover' />
         </div>
         <div>
-          <img src="https://lh3.googleusercontent.com/proxy/JDcMSVaEV-PUlPVLLKR-j86ApQg4l_f1amOu0plPpN3MUSCeIJi_N9v4cj8GjdfNj4fZ9VyS_r-q3c-GYnlA3gOnNMxB0ARN7A8sPux7GDWwWERWRP2ELwRxEKLwsZ8bbjfFFQ" alt="Imagen 2" className='w-full h-64 mb-8 md:h-84  object-cover' />
+          <img src="https://lh3.googleusercontent.com/proxy/JDcMSVaEV-PUlPVLLKR-j86ApQg4l_f1amOu0plPpN3MUSCeIJi_N9v4cj8GjdfNj4fZ9VyS_r-q3c-GYnlA3gOnNMxB0ARN7A8sPux7GDWwWERWRP2ELwRxEKLwsZ8bbjfFFQ" alt="Imagen 2" className='w-full h-44 mb-8 md:h-84  object-cover' />
         </div>
         <div>
-          <img src='https://mir-s3-cdn-cf.behance.net/projects/404/676577108427065.Y3JvcCw4NjIsNjc1LDEzNCww.jpg' alt="Imagen 2" className='w-full h-64 mb-8 md:h-84 object-cover' />
+          <img src='https://mir-s3-cdn-cf.behance.net/projects/404/676577108427065.Y3JvcCw4NjIsNjc1LDEzNCww.jpg' alt="Imagen 2" className='w-full h-44 mb-8 md:h-84 object-cover' />
         </div>
       </Slider>
     </div>
