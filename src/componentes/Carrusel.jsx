ab026c0542b1d504aec19606fd7c1421885c8b62
Facu Sanchez <facusanchz96@gmail.com>
2024-03-17 21:39:49 -0300

    tienda

--- src/componentes/Carrusel.jsx
+++ src/componentes/Carrusel.jsx
@@ -19,17 +19,15 @@ const Carousel = () => {
     <div className="h-64 mb-8 md:h-[40%]">
 
       <Slider {...settings}>
-        <div>
-          <img src="https://www.branca.com.ar/wp-content/uploads/2021/09/Fernet-Branca-Soy-unico_Highlights-01-940x528.png" alt="Imagen 1" className='  w-full h-64 mb-8 md:h-84 object-cover' />
-        </div>
+   
         <div>
           <img src="https://i.ytimg.com/vi/N1NQuyJnWIk/sddefault.jpg" alt="Imagen 2" className='w-full h-64 mb-8 md:h-84 object-cover' />
         </div>
         <div>
-          <img src="https://www.on24.com.ar/wp-content/uploads/2018/05/PALADNI_HAMBRIENTOS-DE-GLORIA.jpg" alt="Imagen 2" className='w-full h-64 mb-8 md:h-84  object-cover' />
+          <img src="https://lh3.googleusercontent.com/proxy/JDcMSVaEV-PUlPVLLKR-j86ApQg4l_f1amOu0plPpN3MUSCeIJi_N9v4cj8GjdfNj4fZ9VyS_r-q3c-GYnlA3gOnNMxB0ARN7A8sPux7GDWwWERWRP2ELwRxEKLwsZ8bbjfFFQ" alt="Imagen 2" className='w-full h-64 mb-8 md:h-84  object-cover' />
         </div>
         <div>
-          <img src='https://i.ytimg.com/vi/6-WPPEZrxds/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGBggZShWMA8=&rs=AOn4CLC5UEIq-z7tVes284mTekDehJNoDg' alt="Imagen 2" className='w-full h-64 mb-8 md:h-84 object-cover' />
+          <img src='https://mir-s3-cdn-cf.behance.net/projects/404/676577108427065.Y3JvcCw4NjIsNjc1LDEzNCww.jpg' alt="Imagen 2" className='w-full h-64 mb-8 md:h-84 object-cover' />
         </div>
       </Slider>
     </div>
